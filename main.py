c3263b42984465c732ff01a0da03a90a4135e22a
FranciscoPG-69 <55768594+FranciscoPG-69@users.noreply.github.com>
2023-09-11 20:17:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+year = int(input("Which year do you want to check? "))
+
+
+result1 = float(year % 4)
+result2 = float(year % 100)
+result3 = float(year % 400)
+
+if result1 == 0:
+    print("Leap year.")
+elif result2 == 0:
+     print("Leap year.")
+else:
+    print("Not leap year.")
