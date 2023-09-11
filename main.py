3d73b2f8b07e73216695d53317c560cac48c45be
FranciscoPG-69 <55768594+FranciscoPG-69@users.noreply.github.com>
2023-09-11 20:36:40 +0000

    segunda solución

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 year = int(input("Which year do you want to check? "))
 
 
-result1 = float(year % 4)
-result2 = float(year % 100)
-result3 = float(year % 400)
-
-if result1 == 0:
-    print("Leap year.")
-elif result2 == 0:
-     print("Leap year.")
+if year % 4 == 0:
+	if year % 100 == 0:
+		if year % 400 == 0:
+			print("Leap year")
+		else:
+			print("Not leap year")
+	else:
+			print("Leap year")	
 else:
-    print("Not leap year.")
-  
+ print("not Leap")
